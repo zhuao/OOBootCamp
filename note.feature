3e9ea3c1b05b211a6b0f38420a1a93281352acba
zhuao <zhuao1051@gmail.com>
2012-10-17 09:19:46 +0800

    create project

---
+++ note.feature
@@ -0,0 +1,3 @@
+GIVEN
+WHEN
+THEN
---
+++ test/com/tw/bootcamp/ParkTest.java
@@ -0,0 +1,11 @@
+package com.tw.bootcamp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuao
+ * Date: 10/16/12
+ * Time: 1:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParkTest {
+}
